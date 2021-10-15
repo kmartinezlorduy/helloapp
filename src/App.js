0cc4493b36e0453a7b933c7dbea061f551eda307
KatherinLorduy <martinezlorduy@gmail.com>
2021-10-14 22:40:26 -0500

    Add file

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ function App() {
         alt = "logo" / >
         <
         p >
-        Edit < code > src / App.js < /code> and save to reload. Katherin Lorduy <
-        /p> <
+        Edit code < code > src / App.js < /code> and save to reload. Katherin Lorduy  / < /code>
+        p > <
         a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
         Learn React <
-        /a> <
-        /header> <
+        /a> < /
+        header > <
         /div>
     );
 }
