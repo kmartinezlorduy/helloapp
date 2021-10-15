c1ecfd1d9704adc6aee4aedc526b45d966a8de3b
KatherinLorduy <martinezlorduy@gmail.com>
2021-10-14 22:47:04 -0500

    jhj

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
+    return (<
+        div className="App" >
         <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
+        header className="App-header" >
+            <
+                img src={logo}
+                className="App-logo"
+                alt="logo" />
+            <
         p >
-        Edit code < code > src / App.js < /code> and save to reload. Katherin Lorduy  / < /code>
+                Edit < code > src / App.js < /code> and save to reload. Katherin Lorduy < /
         p > <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
+        a className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer" >
+                        Learn React <
         /a> < /
         header > <
         /div>
-    );
+                        );
 }
 
-export default App;
+                        export default App;
